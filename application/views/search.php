f1db91b2ab1d91db6840d37fbb132782f2b6e63c
sgnl02 <admin@stefanjonker.nl>
2015-07-12 12:49:58 -0500

    Added search terms their input fields

--- application/views/search.php
+++ application/views/search.php
@@ -1,9 +1,15 @@
 <div class="row">
+	<div class="col-md-12">
+		<h1>Search results</h1>
+	</div>
+
 	<div class="col-md-4">
+		<h2>Search</h2>
 		<?php require 'partial-search.php'; ?>
 	</div>
 
 	<div class="col-md-8">
+		<h2>Results</h2>
 		<?php 
 			foreach($searchResultsArray as $bookArray) {
 				require 'partial-results-books.php';
