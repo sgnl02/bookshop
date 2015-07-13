6acd143bc7d9edf8b9f5d7495521ee15c6910f34
sgnl02 <admin@stefanjonker.nl>
2015-07-12 23:58:29 -0500

    Added favicon

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -13,6 +13,7 @@
     <!-- Bootstrap Core CSS -->
     <?php	echo link_tag('assets/css/bootstrap.min.css'); ?>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="<?php echo base_url('assets/favicon.ico');?>" />
 
     <?php if($this->uri->uri_string() == 'browse' || 'search') {
     echo '
